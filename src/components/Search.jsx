8af6ae2aba6c0ef8bcb7ba900df29e4862648910
Yushenko Dima <damios.1812@gmail.com>
2025-06-06 12:47:45 +0200

    added fix for search of function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class Search extends Component {
     constructor(props) {
@@ -6,73 +6,102 @@ class Search extends Component {
 
         this.state = {
             search: '',
-            filter: 'all'
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+            filter: 'all',
+        };
     }
 
     handleKeyDown = (e) => {
         if (e.key === 'Enter') {
-
             this.props.searchMovies(this.state.search, this.state.filter);
 
             this.setState({
                 search: '',
-                filter: 'all'
-            })
+                filter: 'all',
+            });
         }
-    }
+    };
+    
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
 
     render() {
-
         const { search, filter } = this.state;
 
         return (
-            <div className="row">
-                <div className="col s12">
-                    <div className="input-field">
+            <div className='row'>
+                <div className='col s12'>
+                    <div className='input-field'>
                         <input
-                            className="validate"
-                            placeholder="search"
-                            type="search"
-                            name="search"
+                            className='validate'
+                            placeholder='search'
+                            type='search'
+                            name='search'
                             value={search}
                             onChange={this.handleChange}
                             onKeyDown={this.handleKeyDown}
                         />
-                        <button className="btn search-btn" onClick={() => {this.props.searchMovies(this.state.search, this.state.filter); this.setState({search: '', filter: 'all'})}}>Search</button>
-
+                        <button
+                            className='btn search-btn'
+                            onClick={() => {
+                                this.props.searchMovies(
+                                    this.state.search,
+                                    this.state.filter
+                                );
+                                this.setState({ search: '', filter: 'all' });
+                            }}
+                        >
+                            Search
+                        </button>
                     </div>
-                    <div className="input-field display-flex">
-                        <p className="margin">
+                    <div className='input-field display-flex'>
+                        <p className='margin'>
                             <label>
-                                <input className="with-gap" type="radio" name="filter" value="all" checked={filter === "all"} onChange={this.handleChange}/>
+                                <input
+                                    className='with-gap'
+                                    type='radio'
+                                    name='filter'
+                                    value='all'
+                                    checked={filter === 'all'}
+                                    onChange={this.handleChange}
+                                />
                                 <span>All</span>
                             </label>
                         </p>
 
-                        <p className="margin">
+                        <p className='margin'>
                             <label>
-                                <input className="with-gap" type="radio" name="filter" value="movie" checked={filter === "movie"} onChange={this.handleChange}/>
+                                <input
+                                    className='with-gap'
+                                    type='radio'
+                                    name='filter'
+                                    value='movie'
+                                    checked={filter === 'movie'}
+                                    onChange={this.handleChange}
+                                />
                                 <span>Movies only</span>
                             </label>
                         </p>
 
-                        <p className="margin">
+                        <p className='margin'>
                             <label>
-                                <input className="with-gap" type="radio" name="filter" value="series" checked={filter === "series"} onChange={this.handleChange}/>
+                                <input
+                                    className='with-gap'
+                                    type='radio'
+                                    name='filter'
+                                    value='series'
+                                    checked={filter === 'series'}
+                                    onChange={this.handleChange}
+                                />
                                 <span>Series only</span>
                             </label>
                         </p>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,8 @@
-import { Component } from "react"
+import { Component } from 'react';
 
-import { Cards } from "../components/Cards";
-import { Preloader } from "../components/Preloader";
-import { Search } from "../components/Search";
+import { Cards } from '../components/Cards';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 class Main extends Component {
     constructor(props) {
@@ -12,45 +12,39 @@ class Main extends Component {
             cards: [],
             search: '',
             filter: '',
-        }
-    };
+        };
+    }
 
     componentDidMount() {
         fetch('http://www.omdbapi.com/?i=tt3896198&apikey=377f9cd0&s=matrix')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ cards: data.Search })
-            })
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ cards: data.Search });
+            });
     }
 
-    searchMovies = (value, type) => {
-        if (type === 'all') {
-            fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=377f9cd0&s=${value}`)
-                .then(response => response.json())
-                .then(data => {
-                    this.setState({ cards: data.Search })
-                })
-        } else {
-            fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=377f9cd0&s=${value}&type=${type}`)
-                .then(response => response.json())
-                .then(data => {
-                    this.setState({ cards: data.Search })
-                })
-        }
-    }
+    searchMovies = (value, type = 'all') => {
+        fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=377f9cd0&s=${value}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ cards: data.Search });
+            });
+    };
 
     render() {
-
         const { cards } = this.state;
 
         return (
-            <main className="container content main-content">
+            <main className='container content main-content'>
                 <Search searchMovies={this.searchMovies} />
-                {cards.length ? <Cards cards={cards} /> :
-                    <Preloader />}
+                {cards.length ? <Cards cards={cards} /> : <Preloader />}
             </main>
-        )
+        );
     }
 }
 
-export { Main }
+export { Main };
